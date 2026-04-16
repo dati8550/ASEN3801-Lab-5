e704e3e990b46244fb81e3b657d6482ac621efdb
dati8550 <dati8550@colorado.edu>
2026-04-16 16:15:42 -0600

    Update Lab5_Task3_1.m

--- Lab5_Task3_1.m
+++ Lab5_Task3_1.m
@@ -4,31 +4,48 @@ clc;
 
 % From Task 2 Problem 2
 
-x_trim_0 = [0;      0;      0;
-            0;  0.02780;    0;
-            20.99;  0;   0.5837;
-            0;      0;      0];
-u_trim_0 = [0.1079; 0; 0; 0.3182];
+x_trim_0 = [ ...
+    0;                      % x_e (m)
+    0;                      % y_e (m)
+    -1800;                  % z_e (m) -> Altitude is 1800m
+    0 * (pi/180);           % phi (roll)   -> Converted from 0 deg
+    0.02780;                % theta (pitch)-> ALREADY IN RAD
+    0 * (pi/180);           % psi (yaw)    -> Converted from 0 deg
+    20.99;                  % u (m/s)
+    0;                      % v (m/s)
+    0.5837;                 % w (m/s)
+    0 * (pi/180);           % p (roll rate) -> Converted from 0 deg/s
+    0 * (pi/180);           % q (pitch rate)-> Converted from 0 deg/s
+    0 * (pi/180)            % r (yaw rate)  -> Converted from 0 deg/s
+];
+
+u_trim_0 = [0.1079; 
+            0; 
+            0; 
+            0.3182];
+
 wind_0 = [0;0;0];             % No wind
 
 % Givens
 doublet_time = 0.25;         % seconds
 doublet_size_deg = 15;      % degrees
-doublet_size_rad = doublet_size_deg * (pi/180);
+doublet_size_rad = doublet_size_deg * (pi/180); % Switch to radians
 
-% Function Calls
+% Function Call to get aircraft aparameters
 aircraft_parameters = ttwistor();
 
+h = 1609;
+[~, ~, ~, rho] = stdatmo(h);
+aircraft_parameters.rho = rho;
 
-tspan = [0 3];      % Simulate over 3 seconds
+tspan = 0:0.01:3;      % Simulate over 3 seconds
 
 %% Run ODE 45
-[t_out, x_out] = ode45(@(time, aircraft_state) AircraftEOMDoublet(time, aircraft_state, u_trim_0, doublet_size_rad, doublet_time, wind_0, aircraft_parameters), tspan, x_trim_0);
-
-n = length(t_out);
+options = odeset('MaxStep', 0.01, 'RelTol', 1e-3, 'AbsTol', 1e-3);
+[t_out, x_out] = ode45(@(time, aircraft_state) AircraftEOMDoublet(time, aircraft_state, u_trim_0, doublet_size_rad, doublet_time, wind_0, aircraft_parameters), tspan, x_trim_0, options);n = length(t_out);
 
 for i = 1:n;
-    t = t_oout(i);
+    t = t_out(i);
     de_trim = u_trim_0(1);
     if 0 < t && t <= doublet_time
         de = de_trim + doublet_size_rad;
@@ -42,4 +59,4 @@ for i = 1:n;
 end
 
 fig_num = 1;        
-PlotAircraftSim(t_out, x_out, u_out, fig_num, 'b');
+PlotAircraftSim(t_out, x_out, u_out, 139:144, '-b');
